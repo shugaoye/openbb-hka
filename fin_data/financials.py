6a709bd7edad06963276b2668010a283dbc4a6d6
Roger Ye <shugaoye@yahoo.com>
2025-08-15 10:03:28 +0800

    Completed financials.py

--- fin_data/financials.py
+++ fin_data/financials.py
@@ -7,18 +7,18 @@ def get_balance(ticker: str, period: str, limit: int) -> pd.DataFrame:
     Get balance sheet
     """
     balance_df = obb.equity.fundamental.balance(symbol=ticker, period=period, limit=limit, provider=default_provider).to_dataframe().head(limit)
-    return balance_df[["period_ending", "fiscal_period", "总权益", "负债总额", "总资产"]]
+    return balance_df[["period_ending", "fiscal_period", "股东权益", "总负债", "总资产"]]
 
 def get_cash_flow(ticker: str, period: str, limit: int) -> pd.DataFrame:
     """
     Get cash flow
     """
     cash_flow_df = obb.equity.fundamental.cash(symbol=ticker, period=period, limit=limit, provider=default_provider).to_dataframe().head(limit)
-    return cash_flow_df[["period_ending", "fiscal_period","TOTAL_OPERATE_INFLOW","TOTAL_OPERATE_OUTFLOW","NETCASH_OPERATE","TOTAL_INVEST_INFLOW","TOTAL_INVEST_OUTFLOW","NETCASH_INVEST","TOTAL_FINANCE_INFLOW","TOTAL_FINANCE_OUTFLOW","NETCASH_FINANCE"]]
+    return cash_flow_df[["period_ending", "fiscal_period","营业性现金流","投资性现金流","融资性现金流"]]
 
 def get_income(ticker: str, period: str, limit: int) -> pd.DataFrame:
     """
     Get income statement
     """
     income_df = obb.equity.fundamental.income(symbol=ticker, period=period, limit=limit, provider=default_provider).to_dataframe()
-    return income_df[["TOTAL_OPERATE_INCOME","TOTAL_OPERATE_INCOME_YOY","PARENT_NETPROFIT","PARENT_NETPROFIT_YOY"]].head(limit)
+    return income_df[["总营收","净利润"]].head(limit)
