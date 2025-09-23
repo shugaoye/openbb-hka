a4806445a0284ada6390c0f68c0fe8b9abfb8ba4
Roger Ye <shugaoye@yahoo.com>
2025-09-23 12:02:20 +0800

    Added .idx/ and devserver.sh

---
+++ .idx/dev.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-25.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.python3
+    pkgs.python312Packages.pip
+    pkgs.python312Packages.fastapi
+    pkgs.python312Packages.uvicorn
+    pkgs.uv
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [ "./devserver.sh" ];
+          env = { PORT = "$PORT"; };
+          manager = "web";
+        };
+      };
+    };
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [ "ms-python.python" "rangav.vscode-thunder-client" ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        install =
+          "python -m venv .venv && source .venv/bin/activate &&  pip install -r requirements.txt";
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [ "app.py" ];
+      };
+    };
+  };
+}
---
+++ devserver.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#source .venv/bin/activate
+uv run uvicorn main:app --reload
