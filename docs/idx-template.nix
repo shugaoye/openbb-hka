bddecfc014488547337a4154876cd884bed03b2a
Roger Ye <shugaoye@yahoo.com>
2025-09-24 18:28:38 +0800

    Updated idx-template

--- docs/idx-template.nix
+++ docs/idx-template.nix
@@ -9,12 +9,12 @@
   bootstrap = ''    
     mkdir "$out"
     mkdir -p "$out/.idx/"
-    cp -rf ${./.idx/dev.nix} "$out/.idx/dev.nix"
-    shopt -s dotglob; cp -r ${./core}/* "$out"
-    cp -r ${./fin_data}/* "$out"
-    cp -r ${./routes}/* "$out"
-    cp -r ${./templates}/* "$out"
-    cp ${./main.py} "$out"
+    cp -rf ${../.idx/dev.nix} "$out/.idx/dev.nix"
+    shopt -s dotglob; cp -r ${../core}/* "$out"
+    cp -r ${../fin_data}/* "$out"
+    cp -r ${../routes}/* "$out"
+    cp -r ${../templates}/* "$out"
+    cp ${../main.py} "$out"
     chmod -R +w "$out"
     chmod +rwx "$out/devserver.sh"
   '';
