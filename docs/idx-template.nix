c2fefb8fa39fe804929bd38bc75f061040d66aee
Roger Ye <shugaoye@yahoo.com>
2025-09-25 13:03:50 +0800

    Updated docs/idx-template.nix

--- docs/idx-template.nix
+++ docs/idx-template.nix
@@ -1,20 +1,14 @@
 { pkgs, ... }: {
-  packages = [
-    pkgs.python3
-    pkgs.python312Packages.pip
-    pkgs.python312Packages.fastapi
-    pkgs.python312Packages.uvicorn
-    pkgs.uv
-  ];
   bootstrap = ''    
     mkdir "$out"
     mkdir -p "$out/.idx/"
     cp -rf ${../.idx/dev.nix} "$out/.idx/dev.nix"
-    shopt -s dotglob; cp -r ${../core}/* "$out"
-    cp -r ${../fin_data}/* "$out"
-    cp -r ${../routes}/* "$out"
-    cp -r ${../templates}/* "$out"
+    cp -rf ${../core} "$out"
+    cp -r ${../fin_data} "$out"
+    cp -r ${../routes} "$out"
+    cp -r ${../templates} "$out"
     cp ${../main.py} "$out"
+    cp ${../devserver.sh} "$out"
     chmod -R +w "$out"
     chmod +rwx "$out/devserver.sh"
   '';
